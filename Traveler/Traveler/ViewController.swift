56a727e32f45d75a521e819700848cd406766d1b
GiorgiMachablishvili <g.machablishvili@yahoo.com>
2024-09-02 20:13:26 +0400

    add name label

--- Traveler/Traveler/ViewController.swift
+++ Traveler/Traveler/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private lazy var nameLabel: UILabel = {
+        let view = UILabel(frame: .zero)
+        view.text = "travel up"
+        view.textColor = .black
+        view.textAlignment = .center
+        view.numberOfLines = 1
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setup()
+        layout()
+        
         view.backgroundColor = .white
+    }
+    
+    func setup() {
+        view.addSubview(nameLabel)
+    }
+    
+    func layout() {
+        nameLabel.snp.remakeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(40)
+        }
     }
 }
