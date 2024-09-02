c1d1c649e5be7dfcd43bd10be1d84e18130e8777
Irakli Kochua <ikakochua@gmail.com>
2024-09-02 20:04:47 +0400

    add label from bottom

--- Traveler/Traveler/ViewController.swift
+++ Traveler/Traveler/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    private lazy var titleLabel: UILabel = {
+      let view = UILabel(frame: .zero)
+      view.numberOfLines = 0
+      view.text = "Bottom Traveler"
+      view.textAlignment = .center
+      return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .green
+        
+        setup()
+        layout()
+    }
+    
+    private func setup() {
+        view.addSubview(titleLabel)
+    }
+
+    private func layout() {
+      titleLabel.snp.remakeConstraints { make in
+        make.centerX.equalToSuperview()
+        make.bottom.equalToSuperview().offset(-20)
+      }
     }
 }
